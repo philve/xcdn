b2ea3b53fffac96156ebc06377d417d04bb0e6b3
Philve <47560937+philve@users.noreply.github.com>
2024-01-10 03:33:26 -0800

    Update xcdn.py

--- xcdn.py
+++ xcdn.py
@@ -43,8 +43,8 @@ import re
 class Xcdn(object):
 
     def __init__(self,domain):
-        #必须保证连上了vpn,要在可以ping通google的条件下使用本工具,否则有些domain由于被GFW拦截无法正常访问会导致
-        #本工具判断错误,checkvpn在可以ping通google的条件下返回1
+        # Must be guaranteed to be connected to VPN, and this tool must be used under the conditions that can ping through Google. Otherwise, some Domain will cause
+        #, CheckVPN returns 1
         while 1:
             if checkvpn()==1:
                 break
@@ -55,9 +55,9 @@ class Xcdn(object):
             print("domain format error,make sure domain has no http,like www.baidu.com but not \
 http://www.baidu.com")
             sys.exit(0)
-        #首先保证hosts文件中没有与domain相关的项,有则删除相关
+        #First of all, ensure that there are no items related to Domain in the hosts file, there is deletion related
         domainPattern=domain.replace(".","\.")
-        #下面的sed的正则中不能有\n,sed匹配\n比较特殊
+        #\N, SED matching \n is special
         #http://stackoverflow.com/questions/1251999/how-can-i-replace-a-newline-n-using-sed
         command="sudo sed -ri 's/.*\s+%s//' /etc/hosts" % domainPattern
         os.system(command)
@@ -67,22 +67,22 @@ http://www.baidu.com")
         print('domain的http或https是:%s' % self.http_or_https)
         result=get_request(self.http_or_https+"://"+self.domain,'seleniumPhantomJS')
         self.domain_title=result['title']
-        #下面调用相当于main函数的get_actual_ip_from_domain函数
+       # Below calls the GET_ACTUAL_IP_FROM_DOMain function equivalent to the main function
         actual_ip = self.get_actual_ip_from_domain()
         if actual_ip != 0:
             print("恭喜,%s的真实ip是%s" % (self.domain, actual_ip))
-        #下面用来存放关键返回值
+        #The following is used to store the key return value
         self.return_value=actual_ip
 
         
     def domain_has_cdn(self):
-        # 检测domain是否有cdn
-        # 有cdn时,返回一个字典,如果cdn是cloudflare，返回{'has_cdn':1,'is_cloud_flare':1}
-        # 否则返回{'has_cdn':1,'is_cloud_flare':0}或{'has_cdn':0,'is_cloud_flare':0}
+        # Detect whether Domain has CDN
+        # When there is a CDN, return a dictionary, if the CDN is a cloudflare, return {'Has_cdn': 1, 'is_Cloud_flare': 1}
+        # Otherwise, return {'has_cdn': 1, 'is_cloud_flare': 0} or {'has_cdn': 0, 'is_cloud_flare': 0}
         import re
         CLIOutput().good_print("现在检测domain:%s是否有cdn" % self.domain)
         has_cdn = 0
-        # ns记录和mx记录一样,都要查顶级域名,eg.dig +short www.baidu.com ns VS dig +short baidu.com ns
+        # The NS record is the same as the MX record, and the top domain name must be checked, eg.dig +short www.baidu.com ns dig +short Baidu.com NS
         result = get_string_from_command("dig ns %s +short" % get_root_domain(self.domain))
         pattern = re.compile(
             r"(cloudflare)|(cdn)|(cloud)|(fast)|(incapsula)|(photon)|(cachefly)|(wppronto)|(softlayer)|(incapsula)|(jsdelivr)|(akamai)", re.I)
@@ -95,7 +95,7 @@ http://www.baidu.com")
                 print("has_cdn=1 from ns")
                 return {'has_cdn': 1, 'is_cloud_flare': 0}
         else:
-            # 下面通过a记录个数来判断,如果a记录个数>1个,认为有cdn
+            # The following is judged by the number of records a record.
             result = get_string_from_command("dig a %s +short" % self.domain)
             find_a_record_pattern = re.findall(r"((\d{1,3}\.){3}\d{1,3})", result)
             if find_a_record_pattern:
@@ -109,7 +109,7 @@ http://www.baidu.com")
 
 
     def get_domain_actual_ip_from_phpinfo(self):
-        # 从phpinfo页面尝试获得真实ip
+        # Try to get real IP from PHPINFO page
         CLIOutput().good_print("现在尝试从domain:%s可能存在的phpinfo页面获取真实ip" % self.domain)
         phpinfo_page_list = ["info.php", "phpinfo.php", "test.php", "l.php"]
         for each in phpinfo_page_list:
@@ -123,13 +123,13 @@ http://www.baidu.com")
                 print(each)
                 actual_ip = re.search(r"REMOTE_ADDR[^\.\d]+([\d\.]{7,15})[^\.\d]+", content).group(1)
                 return actual_ip
-        # return 0代表没有通过phpinfo页面得到真实ip
+        # Return 0 means that the real IP is not obtained through the phpinfo page
         return 0
 
 
     def flush_dns(self):
-        # 这个函数用来刷新本地dns cache
-        # 要刷新dns cache才能让修改hosts文件有效
+        # This function is used to refresh the local DNS Cache
+        # DNS Cache can make the modification hosts file effective
         CLIOutput().good_print("现在刷新系统的dns cache")
         command = "service network-manager restart && /etc/init.d/networking force-reload"
         os.system(command)
@@ -138,7 +138,7 @@ http://www.baidu.com")
 
 
     def modify_hosts_file_with_ip_and_domain(self,ip):
-        # 这个函数用来修改hosts文件
+        # This function is used to modify the hosts file
         CLIOutput().good_print("现在修改hosts文件")
         exists_domain_line = False
         with open("/etc/hosts", "r+") as f:
@@ -152,18 +152,18 @@ http://www.baidu.com")
 
 
     def check_if_ip_is_actual_ip_of_domain(self,ip):
-        # 通过修改hosts文件检测ip是否是domain对应的真实ip
-        # 如果是则返回True,否则返回False
-        #CLIOutput().good_print("现在通过修改hosts文件并刷新dns的方法检测ip:%s是否是domain:%s的真实ip" % (ip,self.domain))
-        #python通过requests库或mechanicalsoup库或selenium_phantomjs来请求时不会被dns缓存影响，只会被hosts文件影响dns解析,人工用浏览器访问域名则会受dns缓存影响
+        # To modify whether the IP is the real IP corresponding to Domain
+        # If yes are returned to TRUE, otherwise returns false
+        # Clioutput (). Good_print ("Now to modify the hosts file and refresh the DNS method to detect whether the IP:%s is the real IP"%(IP, SELF.DOMAIN)) of Domain:%s)
+        # Python will not be affected by DNS files when requesting through the REQUESTS library or Mechanicalsoup library or Selenium_Phantomjs. It will only be affected by the Hosts file to analyze DNS.
         CLIOutput().good_print("现在通过修改hosts文件的方法检测ip:%s是否是domain:%s的真实ip" % (ip,self.domain))
         os.system("cp /etc/hosts /etc/hosts.bak")
         self.modify_hosts_file_with_ip_and_domain(ip)
-        #python通过requests库或mechanicalsoup库或selenium_phantomjs来请求时不会被dns缓存影响，只会被hosts文件影响dns解析,人工用浏览器访问域名则会受dns缓存影响
-        #self.flush_dns()
+        # Python will not be affected by the DNS file when requesting through the Requests library or the Mechanicalsoup library or Selenium_Phantomjs. It will only be affected by the Hosts file.
+        # Self.flush_dns ()
         hosts_changed_domain_title= get_request(self.http_or_https + "://%s" % self.domain,'selenium_phantom_js')['title']
         os.system("rm /etc/hosts && mv /etc/hosts.bak /etc/hosts")
-        #这里要用title判断,html判断不可以,title相同则认为相同
+        # It is necessary to judge with title here. HTML is not able to judge that it is not possible. Title is the same.
         if self.domain_title == hosts_changed_domain_title:
             CLIOutput().good_print("检测到真实ip!!!!!!",'red')
             return True
@@ -173,9 +173,9 @@ http://www.baidu.com")
 
 
     def get_c_80_or_443_list(self,ip):
-        # 得到ip的整个c段的开放80端口或443端口的ip列表
+        # Get a list of open port 80 or port 443 IPs for the entire C segment of the IP
         if "not found" in get_string_from_command("masscan"):
-            #这里不用nmap扫描,nmap扫描结果不准
+            # There is no need for nmap scanning, and the nmap scan results are inaccurate
             os.system("apt-get install masscan")
         if self.http_or_https=="http":
             scanPort=80
@@ -187,7 +187,7 @@ http://www.baidu.com")
         os.system(masscan_command)
         with open("/tmp/masscan.out", "r+") as f:
             strings = f.read()
-        #os.system("rm /tmp/masscan.out")
+        # os.system("rm /tmp/masscan.out")
         import re
         allIP=re.findall(r"((\d{1,3}\.){3}\d{1,3})",strings)
         ipList=[]
@@ -198,7 +198,7 @@ http://www.baidu.com")
 
 
     def check_if_ip_c_machines_has_actual_ip_of_domain(self,ip):
-        # 检测ip的c段有没有domain的真实ip,如果有则返回真实ip,如果没有则返回0
+        # Check whether the C segment of the IP address has the real IP address of the domain, return the real IP address if there is one, and return 0 if not
         CLIOutput().good_print("现在检测ip为%s的c段中有没有%s的真实ip" % (ip,self.domain))
         target_list=self.get_c_80_or_443_list(ip)
         for each_ip in target_list:
@@ -208,7 +208,7 @@ http://www.baidu.com")
 
 
     def get_ip_from_mx_record(self):
-        # 从mx记录中得到ip列表,尝试从mx记录中的c段中找真实ip
+        # Get the IP list from the MX record and try to find the real IP address from the C segment in the MX record
         print("尝试从mx记录中找和%s顶级域名相同的mx主机" % self.domain)
         import socket
         # domain.eg:www.baidu.com
@@ -227,8 +227,8 @@ http://www.baidu.com")
 
 
     def check_if_mx_c_machines_has_actual_ip_of_domain(self):
-        # 检测domain的mx记录所在ip[或ip列表]的c段中有没有domain的真实ip
-        # 有则返回真实ip,没有则返回0
+        # Check whether there is a real IP address of the domain in the C segment of the IP [or IP list] where the MX record of the domain is located", 
+        # If there is a real IP, it will return the real IP, and if it does not, it will return 0
         CLIOutput().good_print("尝试从mx记录的c段中查找是否存在%s的真实ip" % self.domain)
         ip_list = self.get_ip_from_mx_record()
         if ip_list != []:
@@ -242,8 +242,8 @@ http://www.baidu.com")
 
 
     def get_ip_value_from_online_cloudflare_interface(self):
-        # 从在线的cloudflare查询真实ip接口处查询真实ip
-        # 如果查询到真实ip则返回ip值,如果没有查询到则返回0
+        # Check the real IP from the online cloudflare in the real IP interface
+        # If you query the real IP, return the IP value, if it is not found, return 0
         CLIOutput().good_print("现在从在线cloudflare类型cdn查询真实ip接口尝试获取真实ip")
         url = "http://www.crimeflare.com/cgi-bin/cfsearch.cgi"
         post_data = 'cfS=%s' % self.domain
@@ -255,8 +255,8 @@ http://www.baidu.com")
 
 
     def get_actual_ip_from_domain(self):
-        # 尝试获得domain背后的真实ip,前提是domain有cdn
-        # 如果找到了则返回ip,如果没有找到返回0
+        # Try to get the real IP behind Domain, provided that Domain has CDN
+        # If you find it, return the IP, if not found to return 0
         CLIOutput().good_print("进入获取真实ip函数,认为每个domain都是有cdn的情况来处理")
         import socket
         has_cdn_value = self.domain_has_cdn()
@@ -267,26 +267,26 @@ http://www.baidu.com")
             CLIOutput().good_print("Attention...!!! Domain doesn't have cdn,I will return the only one ip")
             true_ip = socket.gethostbyname_ex(self.domain)[2][0]
             return true_ip
-        # 下面尝试通过cloudflare在线查询真实ip接口获取真实ip
+        # Let's try to obtain real IP online through Cloudflare online inquiries online
         if has_cdn_value['is_cloud_flare'] == 1:
             ip_value = self.get_ip_value_from_online_cloudflare_interface()
             if ip_value != 0:
                 return ip_value
             else:
                 pass
-        # 下面尝试通过可能存在的phpinfo页面获得真实ip
+        # The following is trying to obtain the real IP through the possible PHPINFO page
         ip_from_phpinfo = self.get_domain_actual_ip_from_phpinfo()
         if ip_from_phpinfo == 0:
             pass
         else:
             return ip_from_phpinfo
-        # 下面通过mx记录来尝试获得真实ip
+        # Let's try to get the real IP through the MX record
         result = self.check_if_mx_c_machines_has_actual_ip_of_domain()
         if result == 0:
             pass
         else:
             return result
-        print("很遗憾,在下认为%s有cdn,但是目前在下的能力没能获取它的真实ip,当前函数将返回0" % self.domain)
+        print("Unfortunately, %s is assumed to have a cdn, but the current capability under %s cannot get its true ip, so the current function will return 0." % self.domain)
         return 0
 
 
